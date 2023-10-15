65c391a4ff8b44f817e5f6f914a31db9a99cb1e6
William <william.jsson@hotmail.com>
2023-10-15 17:52:50 +0200

    Create SentinelNetGuard_Node_Setup.sh

---
+++ SentinelNetGuard_Node_Setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# GitHub repository URL
+GITHUB_REPO="https://github.com/wilarN/SentinelNetGuard/tree/main/server-software/server"
+
+# Installation directory
+INSTALL_DIR="/opt/SentinelNetGuard"
+
+# Custom command to run the main.py script
+CUSTOM_COMMAND="sng"
+
+# Check for root privileges
+if [ "$(id -u)" != "0" ]; then
+  echo "Please run the SentinelNetGuard setup script with sudo or as root."
+  exit 1
+fi
+
+# Python and pip check
+if ! python3 --version &> /dev/null || ! pip3 --version &> /dev/null; then
+  echo "Python3 and pip3 are required. Please install them first."
+  exit 1
+fi
+
+mkdir -p "$INSTALL_DIR"
+
+git clone "$GITHUB_REPO" "$INSTALL_DIR"
+
+chown -R root:root "$INSTALL_DIR"
+chmod -R 755 "$INSTALL_DIR"
+chmod -R o+x "$INSTALL_DIR"
+
+pip3 install -r "$INSTALL_DIR/requirements.txt"
+
+mv "$INSTALL_DIR/sng" "/usr/local/bin/$CUSTOM_COMMAND"
+chmod +x "/usr/local/bin/$CUSTOM_COMMAND"
+
+# Start
+cd "$INSTALL_DIR"
+python3 main.py
